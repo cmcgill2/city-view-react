ad937ed48026d59d0f74f03bfd46c59b7451be96
Chris McGillicuddy <chris@Chriss-iMac.local>
2018-05-17 00:19:19 -0400

    Fixes formatting

--- src/Feature.js
+++ src/Feature.js
@@ -4,11 +4,11 @@ const Feature = ( props ) => {
     return (
         <div className="col-1-of-4">
             <div className="feature-box">
-                    <i className="feature-box__icon icon-basic-world"></i>
-                    <h3 className="heading-tertiary u-margin-bottom-small">{props.city}</h3>
-                    <p className="feature-box__text">
-                                {props.lorem}
-                    </p>
+                <i className="feature-box__icon icon-basic-world"></i>
+                <h3 className="heading-tertiary u-margin-bottom-small">{props.city}</h3>
+                <p className="feature-box__text">
+                {props.lorem}
+                </p>
             </div>
         </div>
     );
