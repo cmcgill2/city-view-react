bf902706ae72f7b5d7e21a5d99f4f48e2d7f9c74
cmcgill2 <cmcgill2@asu.edu>
2018-06-27 16:43:59 -0400

    fixes links

--- src/Footer.js
+++ src/Footer.js
@@ -7,40 +7,27 @@ const Footer = props => {
         <div className="col-1-of-2">
           <div className="footer__navigation">
             <ul className="footer__list">
-              <li className="footer__item">
-                <a href="#" className="footer__link">
+              <li className="footer__item">  
                   Company
-                </a>
               </li>
               <li className="footer__item">
-                <a href="#" className="footer__link">
                   Contact us
-                </a>
               </li>
               <li className="footer__item">
-                <a href="#" className="footer__link">
                   Careers
-                </a>
               </li>
               <li className="footer__item">
-                <a href="#" className="footer__link">
                   Privacy policy
-                </a>
               </li>
               <li className="footer__item">
-                <a href="#" className="footer__link">
                   Terms
-                </a>
               </li>
             </ul>
           </div>
         </div>
         <div className="col-1-of-2">
           <p className="footer__copyright">
-            Design inspired by{" "}
-            <a href="#" className="footer__link">
-              Jonas Schmedtmann
-            </a>. This site is part of a demo project by Chris McGillicuddy.
+             This site is part of a demo project by Chris McGillicuddy.
           </p>
         </div>
       </div>
